5c94cc1161ed53aa0ca0c570607724b96a76279a
HugoMengualAlcGVA <hugmenalc@alu.edu.gva>
2025-11-07 13:46:55 +0100

    dockerfileBack a mitad hacer

---
+++ despliegueSpring/src/main/java/com/example/despliegueSpring/DespliegueSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.despliegueSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DespliegueSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DespliegueSpringApplication.class, args);
+	}
+
+}
---
+++ despliegueSpring/src/test/java/com/example/despliegueSpring/DespliegueSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.despliegueSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DespliegueSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
